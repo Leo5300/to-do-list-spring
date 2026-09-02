b0cbb32e7993b6bbaaecf22924b8b957fe4b477f
LEONARDO MARCHI MALHEIROS <leonardo.malheiros2@senaisp.edu.br>
2026-09-02 20:59:19 -0300

    #PARAOSBETAS

--- src/main/java/br/com/aweb/to_do_list/controller/TodoController.java
+++ src/main/java/br/com/aweb/to_do_list/controller/TodoController.java
@@ -1,31 +1,49 @@
 package br.com.aweb.to_do_list.controller;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.com.aweb.to_do_list.model.Todo;
+import br.com.aweb.to_do_list.repository.TodoRepository;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/todo")
 public class TodoController {
 
-    @GetMapping("home")
-    public ModelAndView home() {
-        var modelAndView = new ModelAndView("home");
-        modelAndView.addObject("professor", "André Roberto da Silva");
-        var alunos = List.of(
-                "Isaac Newton",
-                "Albert Einstein",
-                "Marie Curie");
+    @Autowired
+    TodoRepository todoRepository;
 
-        modelAndView.addObject("alunos", alunos);
+    @GetMapping 
+    public ModelAndView list (){
+       // var ModelAndView = new ModelAndView("list");
+        //ModelAndView.addObject("todos", todoRepository.findAll());
+       // return ModelAndView;
+      // return new ModelAndView("list", Map.of("todos", todoRepository.findAll())
 
-        modelAndView.addObject("ehVerdade", true);
-
-        return modelAndView;
-
+       return new ModelAndView("list", Map.of("todos",
+        todoRepository.findAll(Sort.by("deadline"))));
     }
 
+    @GetMapping("/create")
+    public ModelAndView create() {
+        return new ModelAndView("form", Map.of("todo", new Todo()));
+    }
+
+    @PostMapping("/create")
+    public String create(@Valid Todo todo, BindingResult result) {
+        if (result.hasErrors())
+            return "form";
+        todoRepository.save(todo);
+        return "redirect:/todo";
+    }
 }
