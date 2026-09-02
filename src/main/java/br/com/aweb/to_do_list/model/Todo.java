06763961d4089cfc2fcced5dab4dcffc4181ad7a
LEONARDO MARCHI MALHEIROS <leonardo.malheiros2@senaisp.edu.br>
2026-09-02 18:56:59 -0300

    Projeto inicial2

--- src/main/java/br/com/aweb/to_do_list/model/Todo.java
+++ src/main/java/br/com/aweb/to_do_list/model/Todo.java
@@ -1,11 +1,18 @@
 package br.com.aweb.to_do_list.model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 
 @Entity
 public class Todo {
     private Long id;
     private String title;
-    private LocalDateTime createdAt =
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDate deadline;
+    private LocalDat
+
+    
     
 }
