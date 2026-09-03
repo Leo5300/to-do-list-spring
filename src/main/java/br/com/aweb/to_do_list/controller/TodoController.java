c2a857c37566e6bbf5318ead1c27bd4013c69023
LEONARDO MARCHI MALHEIROS <leonardo.malheiros2@senaisp.edu.br>
2026-09-02 21:21:46 -0300

    #ParaOsParasitas

--- src/main/java/br/com/aweb/to_do_list/controller/TodoController.java
+++ src/main/java/br/com/aweb/to_do_list/controller/TodoController.java
@@ -1,15 +1,17 @@
 package br.com.aweb.to_do_list.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.aweb.to_do_list.model.Todo;
@@ -23,27 +25,50 @@ public class TodoController {
     @Autowired
     TodoRepository todoRepository;
 
-    @GetMapping 
-    public ModelAndView list (){
-       // var ModelAndView = new ModelAndView("list");
-        //ModelAndView.addObject("todos", todoRepository.findAll());
-       // return ModelAndView;
-      // return new ModelAndView("list", Map.of("todos", todoRepository.findAll())
-
-       return new ModelAndView("list", Map.of("todos",
-        todoRepository.findAll(Sort.by("deadline"))));
+    @GetMapping
+    public ModelAndView list() {
+        return new ModelAndView("list",
+                Map.of("todos",
+                        todoRepository.findAll(Sort.by("deadline"))));
     }
 
     @GetMapping("/create")
     public ModelAndView create() {
-        return new ModelAndView("form", Map.of("todo", new Todo()));
+        return new ModelAndView("form",
+                Map.of("todo", new Todo()));
     }
 
     @PostMapping("/create")
     public String create(@Valid Todo todo, BindingResult result) {
+
         if (result.hasErrors())
             return "form";
+
         todoRepository.save(todo);
+
+        return "redirect:/todo";
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@PathVariable Long id) {
+
+        var todo = todoRepository.findById(id);
+
+        if (todo.isPresent())
+            return new ModelAndView("form",
+                    Map.of("todo", todo.get()));
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/edit/{id}")
+    public String edit(@Valid Todo todo, BindingResult result) {
+
+        if (result.hasErrors())
+            return "form";
+
+        todoRepository.save(todo);
+
         return "redirect:/todo";
     }
 }
